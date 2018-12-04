7c62c5127dc9ee00195f50549582fdd10b961b7b
gdouxxx <448052977@qq.com>
2018-12-04 09:01:57 +0800

    cytx

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,10 @@
+package com.pojo;
+
+public class User {
+    private long userId;
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+    private String userPhone;
+    private String UserPhone;
+}
